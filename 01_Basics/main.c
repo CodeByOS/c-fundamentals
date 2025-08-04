e1f236c19d2a995b1aecf4acd2a55c099be57f19
Oussama <saidi.oussama2004@gmail.com>
2025-08-04 18:27:02 +0100

    Initial commit..

---
+++ 01_Basics/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h> //* This called header file is required for input and output functions.
+
+// This is a simple C program that prints "Hello World!" and a custom message.
+
+int main() {
+    printf("Hello World!\n");
+    printf("This is my first c program..!");
+    return 0;
+}
